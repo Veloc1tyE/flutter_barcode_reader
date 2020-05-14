8b4210c297407f6497b513e8fc3a15a247a94425
Billy Jeremijenko <w.jeremijenko@gmail.com>
2020-05-14 22:44:40 +1000

    Let's try this ot

--- android/src/main/kotlin/de/mintware/barcode_scan/OK/ZxingAutofocusScannerView.kt
+++ android/src/main/kotlin/de/mintware/barcode_scan/OK/ZxingAutofocusScannerView.kt
@@ -9,12 +9,10 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView
 class ZXingAutofocusScannerView(context: Context) : ZXingScannerView(context) {
 
     private var callbackFocus = false
-    private var autofocusPresence = false
 
     override fun setupCameraPreview(cameraWrapper: CameraWrapper?) {
         cameraWrapper?.mCamera?.parameters?.let { parameters ->
             try {
-                autofocusPresence = parameters.supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO);
                 parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
                 cameraWrapper.mCamera.parameters = parameters
             } catch (ex: Exception) {
@@ -25,9 +23,6 @@ class ZXingAutofocusScannerView(context: Context) : ZXingScannerView(context) {
     }
 
     override fun setAutoFocus(state: Boolean) {
-        //Fix to avoid crash on devices without autofocus (Issue #226)
-        if(autofocusPresence){
-            super.setAutoFocus(callbackFocus)
-        }
+        super.setAutoFocus(callbackFocus)
     }
 }
